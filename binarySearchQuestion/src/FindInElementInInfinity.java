89064671de66c23bdbf3ecb9e971bfc40f1f0be2
Tejas-pr <Tejas.teju02@gmail.com>
2026-04-08 19:01:02 +0530

    infinity

---
+++ binarySearchQuestion/src/FindInElementInInfinity.java
@@ -0,0 +1,34 @@
+public class FindInElementInInfinity {
+    public static void main(String[] args) {
+        int[] arr = { 3, 4, 5, 6, 7, 8, 9, 11, 22, 33, 44, 55, 66, 77, 88, 99 };
+        int target = 9;
+        System.out.println(ans(arr, target));
+    }
+
+    static int ans(int[] arr, int target) {
+        int start = 0;
+        int end = 1;
+        while (target > arr[end]) {
+            int newStart = end + 1;
+            end = end + (end - start + 1) * 2;
+            start = newStart;
+        }
+
+        return binarySearch(arr, target, start, end);
+    }
+
+    static int binarySearch(int[] arr, int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
