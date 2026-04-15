49e5fd2a95d534bb3d758783828bbb43619838da
Tejas <tejas.teju02@gmail.com>
2026-04-15 13:24:53 +0530

    search in 2d arrray BS

---
+++ binarySearchQuestion/src/RowColMetrics.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class RowColMetrics {
+    public static void main(String[] args) {
+        int[][] arr = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+        };
+        System.out.println(Arrays.toString(search(arr, 1)));
+    }
+
+    static int[] search(int[][] arr, int target) {
+        int r = 0;
+        int c = arr.length - 1;
+
+        while (r < arr.length && c >= 0) {
+            if (arr[r][c] == target) {
+                return new int[] { r, c };
+            }
+
+            if (arr[r][c] < target) {
+                r++;
+            } else {
+                c--;
+            }
+        }
+        return new int[] { -1, -1 };
+    }
+}
