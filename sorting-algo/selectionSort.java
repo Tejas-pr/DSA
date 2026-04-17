82ee963b8bf9637c9140909ab2242b9b74ba626b
Tejas <tejas.teju02@gmail.com>
2026-04-17 09:58:31 +0530

    SORT: selection sort

---
+++ sorting-algo/selectionSort.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class selectionSort {
+    public static void main(String[] args) {
+        int[] arr = { 5, 4, 3, 2, 1 };
+        selection(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void selection(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int end = arr.length - i - 1;
+            int maxEle = getMaxIndex(arr, 0, end);
+            // get the max and swap to the last index.
+            swap(arr, maxEle, end);
+        }
+    }
+
+    static void swap(int[] arr, int x, int y) {
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+    }
+
+    static int getMaxIndex(int[] arr, int start, int end) {
+        int max = start;
+        for (int i = start; i <= end; i++) {
+            if (arr[i] > arr[max]) {
+                max = i;
+            }
+        }
+        return max;
+    }
+}
