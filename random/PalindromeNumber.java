57808c94260666d65ea6f9d41ea4996b096370a7
Tejas-pr <Tejas.teju02@gmail.com>
2026-05-04 20:39:31 +0530

    palin

---
+++ random/PalindromeNumber.java
@@ -0,0 +1,20 @@
+package random;
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        String str = "abbcbba";
+        System.out.println(isPalindrom(str));
+    }
+    static boolean isPalindrom(String str) {
+        str = str.toLowerCase();
+        for(int i=0;i<=str.length()/2;i++) {
+            char start = str.charAt(i);
+            char end = str.charAt(str.length() - i - 1);
+            
+            if(start != end) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
