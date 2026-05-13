c4d3b1d444c53657e1f429310c09934092397eca
Tejas-pr <Tejas.teju02@gmail.com>
2026-05-13 08:31:22 +0530

    two sum fix

--- Blind75/TwoSum.java
+++ Blind75/TwoSum.java
@@ -1,11 +1,14 @@
+import java.util.Arrays;
 import java.util.HashMap;
 // https://leetcode.com/problems/two-sum/?envType=problem-list-v2&envId=oizxjoit
 class TwoSum {
     public static void main(String[] args) {
-
+        int []nums = {2,7,11,15};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
     }
 
-    public int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
